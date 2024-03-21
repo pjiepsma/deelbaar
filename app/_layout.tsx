388e6b5d7e05d66fcfa7b0fe7eba295261384b02
unknown <pieter.iepsma@gmail.com>
2024-03-21 18:31:36 +0100

    setup auth

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,6 +4,26 @@ import * as SplashScreen from "expo-splash-screen";
 import React, { useEffect } from "react";
 import { TouchableOpacity } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
+import { Provider, useAuth } from "./auth/auth";
+import * as SecureStore from "expo-secure-store";
+
+// const APPWRITE_PUBLISHABLE_KEY = process.env.EXPO_PUBLIC_API_KEY;
+const tokenCache = {
+  async getToken(key: string) {
+    try {
+      return SecureStore.getItemAsync(key);
+    } catch (err) {
+      return null;
+    }
+  },
+  async saveToken(key: string, value: string) {
+    try {
+      return SecureStore.setItemAsync(key, value);
+    } catch (err) {
+      return null;
+    }
+  },
+};
 
 export {
   // Catch any errors thrown by the Layout component.
@@ -40,11 +60,23 @@ export default function RootLayout() {
     return null;
   }
 
-  return <RootLayoutNav />;
+  return (
+    <Provider>
+      <RootLayoutNav />
+    </Provider>
+  );
 }
 
 function RootLayoutNav() {
   const router = useRouter();
+  const { isLoaded, user } = useAuth();
+
+  useEffect(() => {
+    if (isLoaded && !user) {
+      router.push("/(modals)/login");
+    }
+  }, [user, isLoaded]);
+
   return (
     <Stack>
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
@@ -53,6 +85,7 @@ function RootLayoutNav() {
         options={{
           presentation: "modal",
           title: "Log in or sign up",
+          animation: "slide_from_bottom",
           headerTitleStyle: {
             fontFamily: "mon-sb",
           },
@@ -63,6 +96,22 @@ function RootLayoutNav() {
           ),
         }}
       />
+      <Stack.Screen
+        name="listing/[id]"
+        options={{ headerTitle: "" }}
+      ></Stack.Screen>
+      <Stack.Screen
+        name="(modals)/booking"
+        options={{
+          presentation: "transparentModal",
+          animation: "fade",
+          headerLeft: () => (
+            <TouchableOpacity onPress={() => router.back()}>
+              <Ionicons name="close-outline" size={24} color="black" />
+            </TouchableOpacity>
+          ),
+        }}
+      ></Stack.Screen>
     </Stack>
   );
 }
