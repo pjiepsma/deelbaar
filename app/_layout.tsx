03d81fa4ad20070693183194d5840a9209e61085
Pieter Iepsma <pieter.iepsma@gmail.com>
2024-12-12 09:48:32 +0100

    return to expo 51 and matching rn, run expo-doctor and clean up _layout.tsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,28 +1,22 @@
-import Entypo from '@expo/vector-icons/Entypo';
-import { BottomSheetModalProvider } from '@gorhom/bottom-sheet';
-import * as Font from 'expo-font';
-import * as NavigationBar from 'expo-navigation-bar';
-import { Stack } from 'expo-router';
-import { useCallback, useEffect, useState } from 'react';
-import { View, StyleSheet } from 'react-native';
-import { GestureHandlerRootView } from 'react-native-gesture-handler';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
-import * as SplashScreen from 'expo-splash-screen';
-import '../reanimated.config';
-import '@azure/core-asynciterator-polyfill';
-import AnimatedSplash from '~/components/AnimatedSplash';
-import Colors from '~/constants/Colors';
-import { AuthProvider } from '~/lib/AuthProvider';
-import { PowerSyncProvider } from '~/lib/powersync/PowerSyncProvider';
+import Entypo from "@expo/vector-icons/Entypo";
+import { BottomSheetModalProvider } from "@gorhom/bottom-sheet";
+import * as Font from "expo-font";
+import * as NavigationBar from "expo-navigation-bar";
+import { Stack } from "expo-router";
+import { useCallback, useEffect, useState } from "react";
+import { View, StyleSheet } from "react-native";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import * as SplashScreen from "expo-splash-screen";
+import "../reanimated.config";
+import "@azure/core-asynciterator-polyfill";
+import AnimatedSplash from "~/components/AnimatedSplash";
+import Colors from "~/constants/Colors";
+import { AuthProvider } from "~/lib/AuthProvider";
+import { PowerSyncProvider } from "~/lib/powersync/PowerSyncProvider";
 
 // Keep the splash screen visible while we fetch resources
 SplashScreen.preventAutoHideAsync();
-
-// Set the animation options. This is optional.
-SplashScreen.setOptions({
-  duration: 1000,
-  fade: true,
-});
 
 const InitialLayout = () => {
   return (
@@ -56,7 +50,7 @@ const App = () => {
   const onLayoutRootView = useCallback(() => {
     if (appIsReady) {
       NavigationBar.setBackgroundColorAsync(Colors.white);
-      SplashScreen.hide();
+      SplashScreen.hideAsync();
     }
   }, [appIsReady]);
 
